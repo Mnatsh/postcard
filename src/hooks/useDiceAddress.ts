1d11ab2b8c30bab40b1e8473035ba9eb82f1fc76
Mnatsh <mnatsh12@gmail.com>
2024-05-06 15:24:13 +0300

    first commit

---
+++ src/hooks/useDiceAddress.ts
@@ -0,0 +1,18 @@
+import { mnemonicToWalletKey } from "@ton/crypto";
+import { WalletContractV4} from "@ton/ton";
+
+export async function useDiceAddress() {
+    // open wallet v4 (notice the correct wallet version here)
+    let mnemonic = <string> process.env.MNEMONIC; // your 24 secret words (replace ... with the rest of the words)
+    // const mnemonic = "unfold sugar water ..."; // your 24 secret words (replace ... with the rest of the words)
+    const key = await mnemonicToWalletKey(mnemonic.split(" "));
+    const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
+  
+    // print wallet address
+    console.log(wallet.address.toString({ testOnly: true }));
+  
+    // print wallet workchain
+    console.log("workchain:", wallet.address.workChain);
+
+
+    }
